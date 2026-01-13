ee7ba4ef5a67a7fc23914734005dcd035970fb1b
Bibek Shrestha <bibekshrestha101@gmail.com>
2026-01-13 16:35:20 -0600

    /ping endpoint

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
 
+# main variable to use.
 app = Flask(__name__)
 
-@app.route('/')
+# start creating end-points.
+
+@app.route("/", methods=['GET'])
 def hello():
-    return 'Hello, World!'
+    return "Hello there.!!"
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return {"message" : "why are you pinging me?"}
